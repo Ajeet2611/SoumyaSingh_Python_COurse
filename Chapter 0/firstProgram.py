3721e50dc0e9e41271cd83c0c4c122c9c0de9928
ajeetkumar <ajeetkumar@AJEET>
2025-11-29 19:01:27 +0530

    multiple time name

--- Chapter 0/firstProgram.py
+++ Chapter 0/firstProgram.py
@@ -1,3 +1,7 @@
 print("hello, world!")
 print("This is my first Python program.")
-print ("my name is ajeet prasad")
+print ("my name is ajeet prasad\n" * 10)
+print("I am learning Python programming.")
+print("Python is fun!")
+
+# This program prints a greeting message to the console.
