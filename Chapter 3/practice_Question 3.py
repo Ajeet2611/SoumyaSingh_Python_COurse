32a7f5f6151b56408406dfad7dcc40a844f649a8
ajeetkumar <ajeetkumar@AJEET>
2025-12-04 00:57:41 +0530

    practice quetion3

---
+++ Chapter 3/practice_Question 3.py
@@ -0,0 +1,4 @@
+str = input("Enter any string:")# Ajeet favorite food is Pizza
+print(str.lower() ) # Output: 'ajeet favorite food is pizza'
+print(str.upper()   )# Output: 'AJEET FAVORITE FOOD IS PIZZA'
+print(str.replace(' ', '_'))  # Output: 'Ajeet_favorite_food_is_Pizza'
