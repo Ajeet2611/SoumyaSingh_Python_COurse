0b6f0c1ba64d1e5c0e63c011c1ba7847e8c06e29
ajeetkumar <ajeetkumar@AJEET>
2025-12-03 18:12:35 +0530

    Smart Temprature Converter System

---
+++ Chapter 2/Assigment/Smart temperature Converter.py
@@ -0,0 +1,5 @@
+input = float(input("Enter temperature in Celsius: "))
+Fahrenheit = (input * 9/5) + 32
+print ("Temperature in Fahrenheit:", Fahrenheit)# Simple Temperature Converter from Celsius to Fahrenheit
+Kelvin = input + 273.15
+print ("Temperature in Kelvin:", Kelvin) # Simple Temperature Converter from Celsius to Kelvin
