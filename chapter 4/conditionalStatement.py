d63939f567ed0acf4e153fb57f3bedad4e769762
ajeetkumar <ajeetkumar@AJEET>
2025-12-04 20:47:38 +0530

    CONDITIONAL STATEMENT

---
+++ chapter 4/conditionalStatement.py
@@ -0,0 +1,14 @@
+#conditional statement.py
+marks = 85
+if (marks>=90):
+    print("your grade is A")
+elif (marks>=80):
+     print("your grade is B")
+
+age = 20
+if (age<18):
+    print("you are a minor")
+else:
+    print("you are an adult")
+
+print("my code end here")
