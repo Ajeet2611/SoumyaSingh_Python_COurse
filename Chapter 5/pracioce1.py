fc06b0a2aff6c02f50b820ed68d6d37e893369a1
ajeetkumar <ajeetkumar@AJEET>
2025-12-06 22:50:18 +0530

    practice 1

---
+++ Chapter 5/pracioce1.py
@@ -0,0 +1,8 @@
+#Create a dictionary named marks to store marks of 3 subjects
+#add the subjects one by one and print the final dictionary
+marks ={}
+print(type(marks))
+marks["Maths"] = 95
+marks["Science"] = 90
+marks["English"] = 85
+print("Marks dictionary is : ",marks)  #{'Maths': 95, 'Science': 90, 'English': 85}
