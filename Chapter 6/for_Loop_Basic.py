9e23f6bf21bd5d0816939a913cce7fe45d983eaa
ajeetkumar <ajeetkumar@AJEET>
2025-12-08 01:54:21 +0530

    for loop

---
+++ Chapter 6/for_Loop_Basic.py
@@ -0,0 +1,10 @@
+foodList =["cake","jalebi","pizza"]
+
+for i in foodList :
+    print(i)
+
+
+collegesList =("NIT -Delhi","ABES","BBAU","IIT-Dehli","IIT_Hydrabad")
+
+for eachitem in collegesList :
+    print("Colleges visited by Saumya di  ", eachitem)
