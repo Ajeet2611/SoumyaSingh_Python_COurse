0e1b4f81c9a00a48de197417e71fad661da518c3
ajeetkumar <ajeetkumar@AJEET>
2025-12-07 20:25:44 +0530

    Create a Set of number and show union and intersection with another set

---
+++ Chapter 5/mini assignment/create_Sets.py
@@ -0,0 +1,10 @@
+#Create a Set of number and show union and intersection with another set
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+print("Set 1 is :", set1)
+print("Set 2 is :", set2)
+# Union of two sets
+print("Union of Set1 and Set2 is : ",set1.union(set2))
+
+# Intersection of two sets
+print("Intersection of Set1 and Set2 is : ",set1.intersection(set2))
