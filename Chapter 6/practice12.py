134eaaf3509253c7cfecaa1461868ccc3566da82
ajeetkumar <ajeetkumar@AJEET>
2025-12-08 12:37:05 +0530

    print 100 to 1

---
+++ Chapter 6/practice12.py
@@ -0,0 +1,6 @@
+#write a program that prints all numbers frome 100 to 1 using for and range().
+#Hint: Use a negative step : range (100 , 0 ,-1)
+print("Print 100 to 1 all numbers ! ")
+for i in range (100, 0 ,-1):
+     print(i)
+
