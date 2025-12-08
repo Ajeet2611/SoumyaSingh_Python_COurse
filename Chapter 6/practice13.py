b7c0e56309471c9db2d26a36ca274980b80633a6
ajeetkumar <ajeetkumar@AJEET>
2025-12-08 12:50:13 +0530

    print name 5 time using loop

---
+++ Chapter 6/practice13.py
@@ -0,0 +1,14 @@
+# Ajeet wants to print her username five times in uppercase letters.
+#write a program to print:
+# Ajeet Prasad
+#Ajeet Prasad
+#Ajeet Prasad
+#Ajeet Prasad
+#Ajeet Prasad
+
+
+name = input("please enter your name:")
+
+for i in range (1,6,1):
+    upper_Case= name.upper()
+    print(upper_Case)
