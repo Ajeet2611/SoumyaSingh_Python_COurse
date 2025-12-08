634d0b394265053577441f563a29d9c5ffe6261f
ajeetkumar <ajeetkumar@AJEET>
2025-12-08 13:03:22 +0530

    print list using for loop

---
+++ Chapter 6/practice14.py
@@ -0,0 +1,6 @@
+#you are given a list of Ajeet's favorite foods. write a python program to print each food item using a for loop.
+
+favoriteFood_List = ["Khichri","Aloo Paratha and dahi","Tarka Dal ","jalebi"]
+for i in favoriteFood_List:
+ print (i)
+ 
