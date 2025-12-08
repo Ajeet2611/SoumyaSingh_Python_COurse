2d7b3df6fa43076690da54b0b3cb0e5a6635e400
ajeetkumar <ajeetkumar@AJEET>
2025-12-08 11:45:40 +0530

    find even number using for loop range function

---
+++ Chapter 6/practice8.py
@@ -0,0 +1,10 @@
+#Write a program using for And range() to print all even numbers between 1 and 20.
+print("even number 1 to 20 :")
+for  num in range(1,20,1):
+    
+    if(num%2==0):
+        
+
+        print(num)
+    
+     
